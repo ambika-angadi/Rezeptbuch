e6150ade9a1c9dd76a306f7fb3dd94639916bb9f
Ambika <rudrambika.angadi@docc.techstarter.de>
2023-07-16 22:03:58 +0200

    edit recipe, delete recipe added

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function openAddRecipeForm() {
 
   // Event listener for the "Add Recipe" link
   document.getElementById('addRecipeLink').addEventListener('click', openAddRecipeForm);
+
+  // Function to open the delete recipe in a new window/tab
+function openAddRecipeForm() {
+  window.open('delete_recipe.html', '_blank', 'width=600,height=400');
+}
+
+// Event listener for the "Delete Recipe" link
+document.getElementById('deleteRecipeLink').addEventListener('click', openAddRecipeForm);
+
+// Function to open the edit recipe form in a new window/tab
+function openAddRecipeForm() {
+  window.open('edit_recipe.html', '_blank', 'width=600,height=400');
+}
+
+// Event listener for the "Edit Recipe" link
+document.getElementById('editRecipeLink').addEventListener('click', openAddRecipeForm);
